78be4e4fd45ef1795132a363dce9709a3087ea33
Jandelson <jandelson_oliveira@yahoo.com.br>
2014-02-17 15:53:57 -0300

    TESTE
    TESTE

--- Monitor/src/TestaConexao.java
+++ Monitor/src/TestaConexao.java
@@ -7,7 +7,7 @@ public class TestaConexao {
 	public static void main(String[] args) throws SQLException {
 
 		Connection conexao = new FabricaDeConexoes().getConnection();
-        JOptionPane.showMessageDialog(null,"Conexao com banco OK","Mensagem",
+        JOptionPane.showMessageDialog(null,"Conexao com banco","Mensagem",
                 JOptionPane.INFORMATION_MESSAGE);
         
         conexao.close();
