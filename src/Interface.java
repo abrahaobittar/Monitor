a21e8e8e013e825b7556860a0b0f27a74e623773
Abrahao Bittar Silva <abrahaobittar@gmail.com>
2014-06-16 11:49:20 -0300

    teste

---
+++ src/Interface.java
@@ -0,0 +1,59 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextArea;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+
+
+public class Interface
+{
+
+	private JFrame frame;
+	private JTextArea textArea;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interface window = new Interface();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public Interface() {
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		textArea = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 0;
+		gbc_textArea.gridy = 0;
+		panel.add(textArea, gbc_textArea);
+	}
+
+	public void setTexto(String texto) {
+		textArea.setText(texto);
+	}
+}
---
+++ src/Monitor.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+public class Monitor implements Runnable
+{
+	public void iniciaMonitor(){
+
+		Path path = Paths.get(setDiretorio("c:\\VAREJO"));
+		WatchService ws = null;
+		Interface ui = new Interface();
+
+		try {
+			ws = FileSystems.getDefault().newWatchService();
+			path.register(ws, StandardWatchEventKinds.ENTRY_CREATE);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		while (true) {
+			WatchKey key = null;
+
+			try {
+				key = ws.take();
+				for (WatchEvent<?> event : key.pollEvents()) {
+					// Kind<?> kind = event.kind();
+					ui.setTexto("Novo arquivo adicionado => "+event.context().toString());
+				//*System.out.println("Novo arquivo inserido => "
+				//+ event.context().toString());
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			boolean reset = key.reset();
+			if (!reset)
+				break;
+		} // Fim while
+	}
+
+	public static String setDiretorio(String dir) {
+		return dir;
+	}
+
+
+} // Fim Classe Main
